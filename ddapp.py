3ee70bf1a927478e7de5869c0478641a8968732c
muddz <mud.dz@live.com>
2014-04-21 13:21:24 +0200

    Cleaned the whole thing some more.

--- ddapp.py
+++ ddapp.py
@@ -29,7 +29,9 @@ __version__ = 0.1
 __date__ = '2014-04-20'
 __updated__ = '2014-04-20'
 
-#The first function that starts, redirects to the different functions of the program
+
+#The first function that starts
+#redirects to the different functions of the program
 def main():
 
     print('''What would you like to do?
@@ -44,7 +46,6 @@ def main():
         choice = 0
         print("That is hardly a number, now is it?")
 
-
     if choice == 1:
         Menu().daremenu()
     elif choice == 2:
@@ -56,16 +57,18 @@ def main():
         main()
 
 
+# class CLIError(Exception):
+#     '''Generic exception to raise and log different fatal errors.'''
+#     def __init__(self, msg):
+#         super(CLIError).__init__(type(self))
+#         self.msg = "E: %s" % msg
+#
+#     def __str__(self):
+#         return self.msg
+#
+#     def __unicode__(self):
+#         return self.msg
 
-class CLIError(Exception):
-    '''Generic exception to raise and log different fatal errors.'''
-    def __init__(self, msg):
-        super(CLIError).__init__(type(self))
-        self.msg = "E: %s" % msg
-    def __str__(self):
-        return self.msg
-    def __unicode__(self):
-        return self.msg
 
 class bcolors:
     HEADER = '\033[95m'
@@ -75,19 +78,26 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+
 # The function that actually throws all the steps on the screen
 class ShowDare:
+
     def show(self, dare):
-        print(bcolors.HEADER + "So, you are going to do the " + dare[2] + " dare. Good choice." + bcolors.ENDC)
+        print(bcolors.HEADER
+              + "So, you are going to do the " + dare[2]
+              + " dare. Good choice." + bcolors.ENDC)
 
         cur.execute("SELECT * FROM steps WHERE dare = (?)", (dare[0],))
         steps = cur.fetchall()
 
         for step in steps:
             print(step[3])
-            raw_input(bcolors.OKGREEN + "Press any key to see what you rolled" + bcolors.ENDC)
-            outcome = random.randrange(1,6)
+            raw_input(bcolors.OKGREEN
+                      + "Press any key to see what you rolled"
+                      + bcolors.ENDC)
+            outcome = random.randrange(1, 6)
             print("Ow, you got a {0}!").format(outcome)
+
 
 class Menu:
 
@@ -98,31 +108,46 @@ class Menu:
         cur.execute("SELECT * FROM 'daretypes'")
         daretypes = cur.fetchall()
 
+        # create a list with dare numbers to check against later.
+        #Might be there is a way more elegant solution for this though
+        darenumbers = []
+
         #Print a row for each of the types
         for daretype in daretypes:
-            print(bcolors.HEADER + "{0} - {1}" + bcolors.ENDC).format(daretype[0], daretype[1])
+            print(bcolors.HEADER
+                   + "{0} - {1}"
+                   + bcolors.ENDC).format(daretype[0], daretype[1])
+
+            # Add the number of the current type to the list
+            darenumbers.append(daretype[0])
 
         # Get the input from the user
         daretype = raw_input("Choose a type of dare by typing a number: ")
         # Check if the input was actually a number
+
         try:
-            #todo: replace this by checking the actual list
             daretype = int(daretype)
         except:
-            print("Got shit in your eyes, you did not enter a number?")
+            print("Got shit in your eyes, you did not enter a number")
+
             sys.exit()
 
-        if daretype <= 6:
+        if daretype in darenumbers:
             # Clear the screen again and display the different dare types
 
             os.system('cls' if os.name == 'nt' else 'clear')
-            cur.execute("SELECT * FROM dares WHERE daretype = (?)", (daretype,))
+            cur.execute(
+                        "SELECT * FROM dares WHERE daretype = (?)",
+                        (daretype,))
             dares = cur.fetchall()
             for dare in dares:
-                print(bcolors.HEADER + "{0} - {1}" + bcolors.ENDC).format(dare[0], dare[2])
+                print(bcolors.HEADER
+                      + "{0} - {1}"
+                      + bcolors.ENDC).format(dare[0], dare[2])
 
             try:
-                dares = raw_input("Which dare would you like from this category?: ")
+                dares = \
+                raw_input("Which dare would you like from this category?: ")
                 os.system('cls' if os.name == 'nt' else 'clear')
             except:
                 print("Well, that is not really a number, now is it?")
